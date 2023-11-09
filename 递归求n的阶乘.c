034654237d9f50e109052198cb7df4ba3ce7dc24
lixixi <146533694+xiaoyi132@users.noreply.github.com>
2023-11-10 02:35:06 +0800

    11.9

---
+++ 递归求n的阶乘.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int Factorial(int n)
+//{
+//	if (n == 1)
+//		return 1;
+//	else
+//	{
+//		return Factorial(n - 1) * n;
+//	} 
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int num = Factorial(n);
+//	printf("%d", num);
+//	return 0;
+//}
